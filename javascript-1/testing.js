5f60679aefbd59e629fc9806f0950cbdfa2dfb89
Giovanni Decenvirale <giovo_adv@hotmail.com>
2023-08-11 14:44:51 -0500

    first commit

---
+++ javascript-1/testing.js
@@ -0,0 +1,20 @@
+/*function carFactory(make, model, year){
+    let carDetails = {
+      make: make,
+      model: model,
+      year: year,
+    }
+    if(year > 2018){
+      carDetails.isNew = true;
+   
+    }else {
+      carDetails.isNew = false;
+    }
+    console.log(carDetails)
+  }
+  carFactory("Toyota", "Tacoma", "2024");*/
+
+  function setGroceryListTitle(x){
+    console.log(`${x}'s Grocery List`);
+}
+  setGroceryListTitle("Giovanni")
---
+++ javascript-2/practice-js-2.js
@@ -0,0 +1,112 @@
+/*
+    For this section of the assessment you will be putting together a cheat sheet for common git commands.  
+    You'll provide the command as well as what it does. 
+*/
+
+//////////////////PROBLEM 1////////////////////
+/*
+    Create a variable called 'gitDefinition'.
+    It should be a string containing your best definition of what Git is.
+*/
+
+//CODE HERE
+
+let gitDefinition = "Git is a DevOps tool used for source code management";
+
+//////////////////PROBLEM 2////////////////////
+/*
+    Create a variable called 'gitHubDefinition'.  
+    It should be a string containing your best definition of what GitHub is.
+*/
+
+//CODE HERE
+let gitHubDefinition = "GitHub is a code hosting platform for version control and collaboration"
+//////////////////PROBLEMS 3 - 9////////////////////
+/*
+    For the next several problems you will be creating objects containing information about different git 
+    commands.  Each object should contain 'description' and 'code' properties.  The 'description' property 
+    will be a string with a description of what that git command does.  The 'code' property should be a 
+    string of the actual command (what you type into your terminal).
+*/
+
+//////////////////PROBLEM 3////////////////////
+/*
+    Create an object called 'init' with 'description' and 'code' properties 
+    following the guidelines above to describe the init command.
+*/
+
+//CODE HERE
+let init = {
+    description: "The git init command creates a new Git repository",
+    code: " git init"
+}
+
+//////////////////PROBLEM 4////////////////////
+/*
+    Create an object called 'clone' with 'description' and 'code' properties 
+    following the guidelines above to describe the clone command.
+*/
+
+//CODE HERE
+let clone = {
+    description: "The git clone command copies an existing Git repository.",
+    code: "git clone"
+}
+
+//////////////////PROBLEM 5////////////////////
+/*
+    Create an object called 'status' with 'description' and 'code' properties 
+    following the guidelines above to describe the status command.
+*/
+
+//CODE HERE
+let status = {
+    description: "The git status command displays the state of the working directory and the staging area",
+    code: "git status"
+}
+//////////////////PROBLEM 6////////////////////
+/*
+    Create an object called 'add' with 'description' and 'code' properties 
+    following the guidelines above to describe the add command.
+*/
+
+//CODE HERE
+let add = {
+    description: "The git add command adds a change in the working directory to the staging area",
+    code: "git add"
+}
+
+//////////////////PROBLEM 7////////////////////
+/*
+    Create an object called 'commit' with 'description' and 'code' properties
+    following the guidelines above to describe the commit command.
+*/
+
+//CODE HERE
+let commit = {
+    description: "The git commit command captures a snapshot of the project's currently staged changes",
+    code: "git commit -m first commit"
+}
+
+//////////////////PROBLEM 8////////////////////
+/*
+    Create an object called 'addRemote' with 'description' and 'code' properties
+    following the guidelines above to describe the command to add a remote location to your git repository.
+*/
+
+//CODE HERE
+let addRemote = {
+    description: "The git remote add command will create a new connection record to a remote repository",
+    code: "git remote add origin"
+}
+//////////////////PROBLEM 9////////////////////
+/*
+    Create an object called 'push' with 'description' and 'code' properties 
+    following the guidelines above to describe the push command.
+*/
+
+//CODE HERE
+let push = {
+    description: "The git push command is used to upload local repository content to a remote repository",
+    code: "git push"
+}
